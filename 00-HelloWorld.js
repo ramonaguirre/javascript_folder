b0864abbda18c97623ace4a07f2afb588fc502bc
Ramón <ramon5130@hotmail.com>
2024-12-03 00:54:16 -0300

    finish 00-helloWorld

--- 00-HelloWorld.js
+++ 00-HelloWorld.js
@@ -1,2 +1,11 @@
 console.log("¡Hola, JavaScript!")
 console.log('¡Hola, JavaScript!')
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
