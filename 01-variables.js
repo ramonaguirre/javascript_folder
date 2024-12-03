547cf3c2c7101e7c9fe3807ec00b9a46da274bf8
Ramón <ramon5130@hotmail.com>
2024-12-03 01:32:59 -0300

    update files in folder

---
+++ 01-variables.js
@@ -0,0 +1,20 @@
+//var
+var helloWorld = "¡Hello, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hello again, JavaScript!"
+console.log(helloWorld)
+
+//let
+let = helloWorld2 = "¡Hello, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hello, JavaScript 2!"
+console.log(helloWorld2)
+
+//const
+const helloWorld3 = "¡Hello, JavaScript 3!"
+console.log(helloWorld3)
+
+helloWorld3 = "¡Hello, JavaScript 3!"
+console.log(helloWorld3)
---
+++ 02-datatypes.js
@@ -0,0 +1,16 @@
+//String (cadena de texto)
+let name = "Chandler Bing"
+let alias = "Chandler"
+let email = 'chandlerfriends@gmail.com'
+
+//Números
+let age = 57
+let height = 1.82
+
+//Booleanos
+let isActor = true
+let isProgrammer = false
+
+//Undefined
+let undefidenValue
+console.log(undefinedValue)
